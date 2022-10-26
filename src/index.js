cf8e9bb3922258637ccc92cd6191bc4bf1bb5737
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-10-26 16:44:53 +0430

    Add functionality to API

--- src/index.js
+++ src/index.js
@@ -6,21 +6,21 @@ const submitBtn = document.getElementById('score-submit-btn');
 const refershBtn = document.querySelector('.refersh-btn');
 const showScores = document.querySelector('.show-scores');
 
-const apiId = '';
-const url = '';
+const apiId = '01GGA325V6WRRYA5ANEAWE4THC';
+const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${apiId}/scores/`;
 
 refershBtn.addEventListener('click', async () => {
   await fetch(url)
     .then((response) => response.json())
-    .then((data) => {
-      data = data.result;
+    .then((gameData) => {
+      gameData = gameData.result;
       showScores.innerHTML = '';
-      data.foEach((element) => {
+      gameData.forEach((element) => {
         const listItem = document.createElement('ul');
         listItem.classList = 'user-score-table';
 
         const userName = document.createElement('li');
-        userName.innerText = element.user;
+        userName.innerText = `${element.user}:`;
 
         const userScore = document.createElement('li');
         userScore.innerText = element.score;
