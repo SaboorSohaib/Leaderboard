caff4e3d94cbe9226fc11b7d3e0c6330456b49d2
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-10-26 13:34:08 +0430

    Add variables

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import './style.css';
-import './modules/data.js';
+
+const playerName = document.getElementById('player-name');
+const playerScore = document.getElementById('player-score');
+const submitBtn  = document.getElementById('score-submit-btn');
+const form = document.getElementById('score-form');
+const refershBtn = document.querySelector('.refersh-btn');
+const scoreList = document.querySelector('.show-scores');
